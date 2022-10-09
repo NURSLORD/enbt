33114f5587779890318f71bfe0dad2624aaedb89
NURSLORD <normosbekov@gmail.com>
2022-10-09 16:18:07 +0600

    changed settings ALLOWED_HOSTS value

--- SAT/settings.py
+++ SAT/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-hs)so(b6jd4+=ewud9fchfzlne2kmnyb71gp1p+7=8d$zyauj(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
